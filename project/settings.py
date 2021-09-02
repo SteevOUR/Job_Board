df54711cd04a6e2c070a8cb95c88a8813c1b4375
SteevOUR <moustafa.ourahho@gmail.com>
2021-09-02 17:52:50 +0100

    up

--- project/settings.py
+++ project/settings.py
@@ -152,7 +152,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ourahho@gmail.com'
-EMAIL_HOST_PASSWORD = 'qvluqjwpubxpnlus'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 
